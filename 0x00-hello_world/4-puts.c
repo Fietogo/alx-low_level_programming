ce13bfb855463ba4a72c7d2c2cd821af388613cb
Fietogo <Justinmackay535@gmail.com>
2023-02-16 14:07:47 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * Main - Entry point
+ * main - Entry point
  * Description - print without 'printf'
- * Return - always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {/*Justin attempting C*/
