ff09598818df455f426c36f4c2c734bd5e441af7
Fietogo <Justinmackay535@gmail.com>
2023-02-16 14:01:06 -0800

    first c code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Main - Entry point
+ * Description - print without 'printf'
+ * Return - always 0
+ */
+int main(void)
+{/*Justin attempting C*/
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
