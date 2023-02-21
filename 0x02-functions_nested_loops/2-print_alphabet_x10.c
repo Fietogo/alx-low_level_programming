875ff2914bc9c177023f514cc5876d907c594a12
Fietogo <Justinmackay535@gmail.com>
2023-02-21 14:42:36 -0800

    so confused

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,16 +3,16 @@
 /**
  * print_alphabet_x10 - print alphabet 10 time in lower case
  */
-void print_alphabet_x10(void)
+int main(void)
 {
 	int count = 0;
 	char letter;
 
-	while (count++ <= 10)
+	while (count++ <= 9)
 	{
-		for (letter = 'a'; letter <= 'z'; letter++)
+		for (letter = 'a'; 'b'; letter <= 'z'; letter++)
 			_putchar(letter);
-		_putchar('/n);
+		_putchar('\n'); letter++;
 	}
 	return (0);
 }
