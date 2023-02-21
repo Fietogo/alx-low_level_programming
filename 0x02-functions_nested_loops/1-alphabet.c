bd158fcc6f1acce576131d057f120d563bdc7838
Fietogo <Justinmackay535@gmail.com>
2023-02-21 12:54:42 -0800

    overwheling

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - Entry point
+ * Description: 'prints the alphabet
+ * in lowercase, followed by a new line'
+ */
+void print_alphabet(void)
+{
+	char alph;
+
+	for (alph = 97; alph <= 122; alph++)
+	{
+		_putchar(alph);
+	}
+	_putchar('\n');
+}
+
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - check for code from Holberton School.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
