629eb749655664c4baeadf6d6b9626e488a1bdba
Fietogo <Justinmackay535@gmail.com>
2023-02-21 17:54:30 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * @num1: integer
+ * @num2: integer
+ * Description: "Write a program that adds integers and returns the result"
+ * Return: Always 0 (Success)
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num@);
+}
