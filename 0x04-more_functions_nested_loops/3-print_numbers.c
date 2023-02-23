a8b0c11dc853c188bb0f193d363b6e292b15554e
Fietogo <Justinmackay535@gmail.com>
2023-02-23 13:55:45 -0800

    push

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,15 @@
 #include "main.h"
 /**
- * print_nummbers - prints nubers from 0 to 9 followed by new line
- * Return: Void
+ * print_numbers - prints nubers from 0 to 9 followed by new line
+ * Description: 'Write a function that prints the numbers
+ * from 0 to 9 followed by a new line
+ * Return: Always 0 (Success)
  */
-void print_number(void)
+void print_numbers(void)
 {
 	int i;
 
-	for (i = 48; i < 58; i++)
-		_putchar("%d", i);
+	for (i = 0; i < 10; i++)
+		_putchar(i + '0');
 	_putchar("\n");
 }
