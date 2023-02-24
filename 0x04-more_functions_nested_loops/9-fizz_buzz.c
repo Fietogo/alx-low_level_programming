4bef0d19e60f14ac0069538daed34f8a0e7867cd
Fietogo <Justinmackay535@gmail.com>
2023-02-24 13:59:00 -0800

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,19 +10,11 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 15 == 0)
-			printf("FizzBuzz\t");
-
-		else if
-			((i % 3) == 0);
-		printf("Fizz\t");
-
-		else if
-			((i % 5) == 0);
-		printf("Buzz\t");
-
-		else
-			printf("%d\t", i)
+		(i % 3 == 0) ? printf("Fizz") : 0;
+		(i % 5 == 0) ? printf("Buzz") : 0;
+		(i % 3 != 0 && i % 5 != 0) ? printf("%d", i) : 0;
+		(i != 100) ? printf(" ") : 0;
 	}
+	printf('\n');
 	return (0);
 }
