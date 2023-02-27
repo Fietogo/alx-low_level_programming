293a39f67015dc8f525c2c55e55f72cb91fe7895
Fietogo <Justinmackay535@gmail.com>
2023-02-27 15:49:58 -0800

    print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_array - Entry point
+ * @a: array
+ * @n: number of elements
+ * Prototype: void print_array(int *a, int n)
+ */
+
+void print_array(int *a, int n)
+{
+	int j;
+
+	for (j = 1; j < n; j++)
+	{
+		printf("d%", a[j]);
+		(j < n - 1) ? printf(", ") : 0;
+	}
+
+	printf('\n');
+}
