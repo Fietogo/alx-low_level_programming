53b0864ee4c85a7c0052980070a4183ed771a21c
Fietogo <Justinmackay535@gmail.com>
2023-03-02 08:13:25 -0800

    push

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncat - Entry point
+ * @dest: string
+ * @src: String
+ * @n: integer
+ * Desctription: 'Write a function that concatenates two strings'
+ * Return: Return a pointer to the resulting string des
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int j = 0, dest_len = 0;
+
+	while (dest[j++])
+		dest_len++;
+
+	for (j = 0, src[j] && j < n; j++)
+		dest[dest_len++] = src[j];
+
+	return (dest);
+}
