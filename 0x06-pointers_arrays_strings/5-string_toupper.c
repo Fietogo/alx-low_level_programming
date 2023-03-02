61fecc9cbcbd592daecdf86829d117c7f36932c2
Fietogo <Justinmackay535@gmail.com>
2023-03-02 12:47:36 -0800

    push

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -12,8 +12,8 @@ char *string_toupper(char *str)
 
 	for (j = 0; str[j] != '\0'; j++)
 	{
-		if (str[j] <= 122 && str [j] >= 97)
-			str[j] = str [j] - 32;
+		if (str[j] <= 122 && str[j] >= 97)
+			str[j] = str[j] - 32;
 		else
 			str[j] = str[j];
 	}
