51e92830066f3534608a095370b463591730a48d
Fietogo <Justinmackay535@gmail.com>
2023-03-02 13:16:26 -0800

    tired

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * leet - Entry point
+ * @str: string
+ *
+ * Return: Pointer to string
+ */
+
+char *leet(char *str)
+{
+	int n, j;
+	char subs[] = "aAeEoOtTlL";
+	char le[] = 43071
+
+	n = 0;
+	for (n = 0. str[n] != '\0; n++)
+	{
+		for (j = 0; subs[j]; != '\0'; j++)
+			if (str[n] == subs[j])
+				str[n] = le[j / 2];
+	}
+	return (str);
+}
+
