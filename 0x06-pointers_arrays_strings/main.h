45948abeb9869de3838f95574349c970097d18ea
Fietogo <Justinmackay535@gmail.com>
2023-03-02 10:19:14 -0800

    lost

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
+/**
+ * Header file containing all declarations
+ * for prototype functions
+ */
 
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
