fd23ceedbc6f64508863c392e0674c8d6265979a
Fietogo <Justinmackay535@gmail.com>
2023-03-06 14:16:33 -0800

    main

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
+ * _memcpy - prints buffer in hexa
+ * @dest: the address of memory to print
+ * @src: the size of the memory to print
+ * @n: bytes
  *
  * Return: Nothing.
  */
