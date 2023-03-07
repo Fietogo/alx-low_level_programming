6104ce1cbbc9dc8c5c9d57942b350e832f7766cb
Fietogo <Justinmackay535@gmail.com>
2023-03-06 16:26:34 -0800

    atleast

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Prints chessboard
+ * @a: chessboard
+ *
+ * Return: Always 0 (success)
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int row, column;
+
+	for (row = 0; row < 8; row++)
+	{
+		for (column = 0; column < 8; column++)
+		{
+			_putchar(a[row][column]);
+		}
+		_putchar('\n');
+	}
+}
+
