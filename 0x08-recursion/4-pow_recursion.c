9ca68cf0c699e0d92be1244f367408ce08b0b8e5
Fietogo <Justinmackay535@gmail.com>
2023-03-09 13:58:18 -0800

    betty

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns x to power of y
+ * @x: number raised
+ * @y: Power of
+ *
+ * Return: x to por of y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < o)
+		return (-1);
+
+	if (y == 0)
+		return (1);
+
+	Return(x * _pow_recursion(x, y - 1));
+}
