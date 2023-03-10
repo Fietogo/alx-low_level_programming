c04a9c777512dce7cc0f0af5ee906857cfc95706
Fietogo <Justinmackay535@gmail.com>
2023-03-10 09:46:06 -0800

    panda

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,7 +8,7 @@
  * Return: Always 0 (Succcess)
  */
 
-int main(int_attribute_((_unused)_) argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 
