7f20d42e1e2bbf6f73d1dcb57d6d3148cbbf5c8d
Fietogo <Justinmackay535@gmail.com>
2023-06-14 05:09:45 -0700

    push

---
+++ 0-whatsmyname.c/0-whatismyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints name
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int _attribute_((_unused_)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
+
