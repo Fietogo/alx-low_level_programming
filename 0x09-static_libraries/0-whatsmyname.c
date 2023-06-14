8217f3264256da09e39326e9fa068f0fc807e9fb
Fietogo <Justinmackay535@gmail.com>
2023-06-14 05:44:16 -0700

    push

---
+++ 0x09-static_libraries/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints name
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%\n", argv [0]);
+
+	return (0);
+}
